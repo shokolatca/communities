47b74e2321913a00f159bf19a0377fb6ae52c421
shokolatca <168018475+shokolatca@users.noreply.github.com>
2024-10-10 22:48:39 +0300

    Update spectral_clustering.py

--- communities/algorithms/spectral_clustering.py
+++ communities/algorithms/spectral_clustering.py
@@ -35,55 +35,76 @@ def eigenvector_matrix(L, n):
 #################
 
 
-def init_communities(num_nodes, k):
-    # QUESTION: Forgy method vs. Random Partition method?
-    return [{i} for i in random.sample(range(num_nodes), k)]
+def init_communities(num_nodes, k, cluster_size):
+    # Инициализация кластеров с фиксированным количеством элементов
+    all_indices = list(range(num_nodes))
+    random.shuffle(all_indices)  # Перемешиваем индексы
+    communities = [set() for _ in range(k)]  # Создаем пустые кластеры
+    return communities
 
 
 def calc_centroids(V, communities):
     centroids = []
     for community in communities:
-        centroid = V[list(community)].mean(axis=0)
+        if len(community) > 0:  # Проверяем, что кластер не пуст
+            centroid = V[list(community)].mean(axis=0)
+        else:
+            # Если кластер пуст, создаем фиктивный нулевой центроид
+            centroid = np.zeros(V.shape[1])
         centroids.append(centroid)
 
     C = np.vstack(centroids)
     return C
 
 
-def update_assignments(V, C, communities):
-    for i in range(len(V)):
-        best_sim, best_comm_index = -1, 0
+def update_assignments(V, C, communities, cluster_size):
+    num_nodes = len(V)
+    # Используем список для отслеживания количества узлов в каждом кластере
+    cluster_sizes = [len(comm) for comm in communities]
+
+    for i in range(num_nodes):
+        best_sim, best_comm_index = -1, None
+
+        # Для каждого узла находим ближайший центроид, учитывая, что кластер не переполнен
         for c_i in range(len(C)):
-            cosine_sim = np.dot(V[i], C[c_i])
-            cosine_sim /= np.linalg.norm(V[i]) * np.linalg.norm(C[c_i])
-            if cosine_sim < best_sim:
+            if cluster_sizes[c_i] >= cluster_size:
+                # Пропускаем кластер, если он уже достиг максимального размера
                 continue
 
-            best_sim = cosine_sim
-            best_comm_index = c_i
+            cosine_sim = np.dot(V[i], C[c_i])
+            cosine_sim /= (np.linalg.norm(V[i]) * np.linalg.norm(C[c_i]))
 
-        communities[best_comm_index].add(i)
+            if cosine_sim > best_sim:
+                best_sim = cosine_sim
+                best_comm_index = c_i
+
+        # Назначаем узел в лучший кластер
+        if best_comm_index is not None:
+            communities[best_comm_index].add(i)
+            cluster_sizes[best_comm_index] += 1
 
     return communities
 
 
-######
+###### 
 # MAIN
 ######
 
 
-def spectral_clustering(adj_matrix : np.ndarray, k : int) -> list:
+def spectral_clustering(adj_matrix: np.ndarray, k: int, cluster_size: int) -> list:
     L = laplacian_matrix(adj_matrix)
     V = eigenvector_matrix(L, k)
 
-    communities = init_communities(len(adj_matrix), k)
+    # Инициализация кластеров
+    communities = init_communities(len(adj_matrix), k, cluster_size)
+    
     while True:
         C = calc_centroids(V, communities)
-        updated_communities = update_assignments(V, C, deepcopy(communities))
+        updated_communities = update_assignments(V, C, deepcopy(communities), cluster_size)
 
         if updated_communities == communities:
             break
 
-        communities = updated_communities
+        communities = deepcopy(updated_communities)
 
     return communities
